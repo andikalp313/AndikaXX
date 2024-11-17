13f5840d6fafb0eb9d49787fa9b35c94fb149346
andikalp313 <andikaluhurpambudi@gmail.com>
2024-11-17 12:45:30 +0700

    repair

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,30 @@
 import { getEntries } from "@/lib/contentful";
 import HomePage from "@/features/home/page";
-import { div } from "framer-motion/client";
 
 interface HomeProps {
   searchParams: { [key: string]: string };
 }
 
 export default async function Home({ searchParams }: HomeProps) {
-  const blogs = await getEntries(Number(searchParams.page) || 1);
+  try {
+    const page = Number(searchParams.page) || 1;
+    const blogs = await getEntries(page);
 
-  if (!blogs) {
-    return;
-  }
-  return (
-    <div>
-      <div className="flex items-center justify-center mx-auto">
-        <HomePage />
+    if (!blogs) {
+      return <div>Tidak ada blog yang ditemukan.</div>;
+    }
+
+    return (
+      <div>
+        <div className="flex items-center justify-center mx-auto">
+          <HomePage />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error("Error fetching blogs:", error);
+    return (
+      <div>Terjadi kesalahan saat memuat data. Silakan coba lagi nanti.</div>
+    );
+  }
 }
